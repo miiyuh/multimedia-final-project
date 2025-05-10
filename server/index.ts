3d4c8148f69ff97c934d56e223c62aa18945fbfe
stelle <itsazripp2@gmail.com>
2025-05-10 21:19:36 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
   const port = 5070;
   server.listen({
     port,
-    host: "127.0.0.1",
+    host: "localhost",
     reusePort: false,
   }, () => {
     log(`serving on port ${port}`);
